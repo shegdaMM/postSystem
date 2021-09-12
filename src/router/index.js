65b5c0a294166b7d8014fa810efe1152c453f217
Shegda Mihail <shegda.mihail@gmail.com>
2021-09-12 09:18:14 +0300

    start

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import LogIn from '../views/LogIn.vue';
+import Registration from '@/views/Registration';
 
 const routes = [
   {
@@ -8,18 +10,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: Registration
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Registration.vue
@@ -0,0 +1,17 @@
+<template>
+  ...
+</template>
+
+<script>
+// https://jasonwatmore.com/post/2020/10/01/vue-3-veevalidate-form-validation-example
+
+export default {
+    name: 'Registration'
+
+};
+
+</script>
+
+<style>
+
+</style>
